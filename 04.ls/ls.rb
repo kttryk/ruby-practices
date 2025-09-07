b1e19e8f1eaba04b19d8a7034d5d5cbfdbacb85a
kttryk <yuto99992@gmail.com>
2025-09-07 22:14:02 +0900

    create ls.rb

---
+++ 04.ls/ls.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+COLUMN_NUM = 3
+
+def get_max_length(array)
+  array.max_by(&:length).length
+end
+
+def create_cols_array(array, column_num)
+  row_num = (array.size + column_num - 1) / column_num
+  cols = Array.new(column_num)
+  (0..column_num - 1).each do |col_index|
+    cols[col_index] = array.slice(row_num * col_index, row_num) || []
+  end
+  cols
+end
+
+def print_array_column(array, column_num)
+  cols = create_cols_array(array, column_num)
+  cols_width = cols.map { |col| get_max_length(col) }
+
+  cols[0].each_index do |row_index|
+    cols.each_index do |col_index|
+      printf("%-#{cols_width[col_index]}s\t", cols[col_index][row_index])
+    end
+    puts
+  end
+end
+
+def get_directory_data(path)
+  Dir.glob("#{path}/*").map do |file|
+    File.basename(file)
+  end
+end
+
+pwd_path = Dir.pwd
+array = get_directory_data(pwd_path)
+print_array_column(array, COLUMN_NUM)
