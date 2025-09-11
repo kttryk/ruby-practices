d659b6c9674cddc1797ee87ea2ca16b72a4f783c
kttryk <yuto99992@gmail.com>
2025-09-12 00:14:24 +0900

    refactoring cal.rb

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -14,34 +14,27 @@ def parse_options
   options
 end
 
-def create_calendar_string_header(year, month)
-  format("     %<month>2d月 %<year>d\n", month: month, year: year)
-end
-
-def create_weekdays_string
-  "日 月 火 水 木 金 土\n"
-end
-
-def create_days_string(year, month)
-  first_day_wday = Date.new(year, month, 1).wday
+def create_days(year, month)
+  first_wday_of_month = Date.new(year, month, 1).wday
   last_day_of_month = Date.new(year, month, -1).day
-  days_string = ' ' * 3 * first_day_wday
-  [*1..last_day_of_month].each do |day|
-    days_string += format('%2d ', day)
-    days_string += "\n" if Date.new(year, month, day).saturday?
+  days = ' ' * 3 * first_wday_of_month
+  (1..last_day_of_month).each do |day|
+    days += format('%2d ', day)
+    days += "\n" if Date.new(year, month, day).saturday?
   end
-  days_string
+  days
 end
 
-def create_calendar_string(year, month)
-  calendar_string = ''
-  calendar_string += create_calendar_string_header(year, month)
-  calendar_string += create_weekdays_string
-  calendar_string += create_days_string(year, month)
-  calendar_string
+def create_calendar(year, month)
+  calendar = ''
+  calendar += format("     %<month>2d月 %<year>d\n", month: month, year: year)
+  calendar += "日 月 火 水 木 金 土\n"
+  calendar += create_days(year, month)
+  calendar
 end
 
 options = parse_options
-year  = options[:year]  || Date.today.year
-month = options[:month] || Date.today.month
-puts create_calendar_string(year, month)
+today = Date.today
+year  = options[:year]  || today.year
+month = options[:month] || today.month
+puts create_calendar(year, month)
