d374c969dcfb8e8813a06f1728de2afc5d70bee4
kttryk <yuto99992@gmail.com>
2025-09-11 23:57:10 +0900

    if in put

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 (1..20).each do |num|
-  if (num % 3).zero? && (num % 5).zero?
-    puts "FizzBuzz"
-  elsif (num % 3).zero?
-    puts "Fizz"
-  elsif (num % 5).zero?
-    puts "Buzz"
-  else
-    puts num
-  end
+  puts(
+    if (num % 3).zero? && (num % 5).zero?
+      "FizzBuzz"
+    elsif (num % 3).zero?
+      "Fizz"
+    elsif (num % 5).zero?
+      "Buzz"
+    else
+      num
+    end
+  )
 end
