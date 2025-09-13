247213234bd8f3be59b04bb09c68a6fe1998ee81
kttryk <yuto99992@gmail.com>
2025-09-13 21:26:09 +0900

    change row col

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -15,26 +15,28 @@ def list_entries
 end
 
 def print_entries(entries)
-  entries_table = create_table(entries)
-  col_widths = entries_table.map { |entries| get_max_length(entries) }
+  row_num = (entries.size + COLUMN_NUM - 1) / COLUMN_NUM
+  entries_table = create_table(entries, row_num)
+  col_widths = get_col_widths(entries, row_num)
 
-  entries_table[0].each_index do |row_index|
-    entries_table.each_index do |col_index|
-      printf("%-#{col_widths[col_index]}s\t", entries_table[col_index][row_index])
+  entries_table.each do |row_entries|
+    row_entries.each_index do |col_index|
+      printf("%-#{col_widths[col_index]}s\t", row_entries[col_index])
     end
     puts
   end
 end
 
-def create_table(entries)
-  row_num = (entries.size + COLUMN_NUM - 1) / COLUMN_NUM
-  Array.new(COLUMN_NUM) do |col_index|
-    entries.slice(row_num * col_index, row_num) || []
+def create_table(entries, row_num)
+  Array.new(row_num) do |row_index|
+    Array.new(COLUMN_NUM) { |col_index| entries[col_index * row_num + row_index] || '' }
   end
 end
 
-def get_max_length(entries)
-  entries.max_by(&:length)&.length || 0
+def get_col_widths(entries, row_num)
+  Array.new(COLUMN_NUM) do |col_index|
+    entries.slice(row_num * col_index, row_num).max_by(&:length)&.length
+  end
 end
 
 main
