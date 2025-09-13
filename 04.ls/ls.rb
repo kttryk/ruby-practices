60a48fabdc264249a2e1b05f27523a92f4def9a1
kttryk <yuto99992@gmail.com>
2025-09-13 20:20:15 +0900

    rename to entries

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -16,7 +16,7 @@ end
 
 def print_entries(entries)
   entries_table = create_table(entries)
-  col_widths = entries_table.map { |col| get_max_length(col) }
+  col_widths = entries_table.map { |entries| get_max_length(entries) }
 
   entries_table[0].each_index do |row_index|
     entries_table.each_index do |col_index|
@@ -26,15 +26,15 @@ def print_entries(entries)
   end
 end
 
-def create_table(array)
-  row_num = (array.size + COLUMN_NUM - 1) / COLUMN_NUM
+def create_table(entries)
+  row_num = (entries.size + COLUMN_NUM - 1) / COLUMN_NUM
   Array.new(COLUMN_NUM) do |col_index|
-    array.slice(row_num * col_index, row_num) || []
+    entries.slice(row_num * col_index, row_num) || []
   end
 end
 
-def get_max_length(array)
-  array.max_by(&:length)&.length || 0
+def get_max_length(entries)
+  entries.max_by(&:length)&.length || 0
 end
 
 main
